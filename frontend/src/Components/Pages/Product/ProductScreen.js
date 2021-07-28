b2b848e1c957cc52a358d58c5bc4f4d6c1dc7504
luciana-pereira <luci.lv14@gmail.com>
2021-07-27 21:41:35 -0300

    Adding Redux to ProductScreen.

--- frontend/src/Components/Pages/Product/ProductScreen.js
+++ frontend/src/Components/Pages/Product/ProductScreen.js
@@ -1,70 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../Product/Actions/ProductActions';
+import Loading from '../../Loading/Loading';
+import Message from '../../Message/Message';
 import Rating from '../../Rating/Rating';
-import data from '../../../data'
-import Img from '../../../Assets/1.jpeg'
+import Img from '../../../Assets/1.jpeg';
 
 
 const ProductScreen = (props) => {
-    const product = data.products.find((x) => x._id === props.match.params.id);
-    if (!product) {
-      return <div> Produto não encontrado</div>;
-    }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
     return (
       <div>
-        <Link to="/">⬅️ Voltar</Link>
-        <div className="row top">
-          <div className="col-2">
-            <img className="large" src={Img} alt={product.name}></img>
-          </div>
-          <div className="col-1">
-            <ul>
-              <li>
-                <h1>{product.name}</h1>
-              </li>
-              <li>
-                <Rating
-                  rating={product.rating}
-                  numReviews={product.numReviews}
-                ></Rating>
-              </li>
-              <li>Preço : ${product.price}</li>
-              <li>
-                Descrição :
-                <p>{product.description}</p>
-              </li>
-            </ul>
-          </div>
-          <div className="col-1">
-            <div className="card card-body">
+        {loading ? (
+        <Loading></Loading>
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <div>
+          <Link to="/">⬅️ Voltar</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img
+                className="large"
+                src={Img}
+                alt={product.name}
+              ></img>
+            </div>
+            <div className="col-1">
               <ul>
                 <li>
-                  <div className="row">
-                    <div>Preço</div>
-                    <div className="price">${product.price}</div>
-                  </div>
+                  <h1>{product.name}</h1>
                 </li>
                 <li>
-                  <div className="row">
-                    <div>Status</div>
-                    <div>
-                      {product.countInStock > 0 ? (
-                        <span className="success">Em Estoque</span>
-                      ) : (
-                        <span className="danger">Indisponível</span>
-                      )}
-                    </div>
-                  </div>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  ></Rating>
                 </li>
+                <li>Pirce : R${product.price}</li>
                 <li>
-                  <button className="primary block">Adicionar Cartão</button>
+                  Description:
+                  <p>{product.description}</p>
                 </li>
               </ul>
             </div>
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    <div className="row">
+                      <div>Price</div>
+                      <div className="price">R${product.price}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Status</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className="success">Em Estoque</span>
+                        ) : (
+                          <span className="danger">Indisponível</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button className="primary block">Adicionar Cartão</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            </div>
           </div>
-        </div>
+      )}
       </div>
-    )
-}
+    );
+  }
 
 export default ProductScreen;
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,10 +1,14 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './Components/Pages/Product/Reducers/ProductReducers';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './Components/Pages/Product/Reducers/ProductReducers';
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
