1c8025992e9e22d6b43bf2cd345f88d511abe82e
luciana-pereira <luci.lv14@gmail.com>
2021-07-30 22:34:34 -0300

    Users in MongoDB.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-undef */
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import UserRouter from './Router/UserRouter.js';
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/grifafro', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -17,8 +25,14 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.use('/api/users', UserRouter);
+
 app.get('/', (req, res) => {
   res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
